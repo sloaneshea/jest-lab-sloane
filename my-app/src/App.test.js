ae3b396186c6272ad47b0535e06ea3170be31ec0
sloaneshea <sloaneshea@gmail.com>
2023-07-20 18:20:38 -0700

    new enter button exists test

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,8 +1,22 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import userEvent from "@testing-library/user-event"
 
-test('renders learn react link', () => {
+test('Class: checking checkbox function', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  //Click on checkbox
+  userEvent.click(screen.getByTestId("checkbox"));
+
+  expect(screen.getByTestId("checkbox")).toBeChecked()
+  // Checking if checkbox is not checked
+});
+
+test('Contains enter button', () => {
+  render(<App />);
+
+  //get enter button button by attribute and name
+  const enterButton = screen.queryByRole('button', { name: 'Enter' });
+
+  //make sure it exists on page
+  expect(enterButton).toBeInTheDocument;
 });
